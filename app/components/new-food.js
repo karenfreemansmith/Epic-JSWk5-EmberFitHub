b0e7046df384542c2310eb60fd3f48a311cdf99f
Alex Jacks and Garrett Price <alex+garrett@>
2016-11-07 15:49:05 -0800

    builds working Add New Food component to Food route

---
+++ app/components/new-food.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  model() {
+    return this.store.findAll('food')
+  },
+  actions: {
+    addNewFood() {
+     var params = {
+       food: this.get('new-food') ? this.get('new-food') : "",
+       calories: this.get('new-calories') ? this.get('new-calories') : "",
+       protein: this.get('new-protein') ? this.get('new-protein') : "",
+       carbs: this.get('new-carbs') ? this.get('new-carbs') : "",
+       fat: this.get('new-fat') ? this.get('new-fat') : "",
+       category: this.get('new-category') ? this.get('new-category') : "",
+     };
+     this.sendAction('addNewFood', params);
+   }
+ }
+});
--- app/routes/foods.js
+++ app/routes/foods.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('food')
+  },
+  actions: {
+    addNewFood(params) {
+      var newFood = this.store.createRecord('food', params);
+      newFood.save();
+      console.log(params);
+      this.transitionTo('foods');
+    }
+  }
+
+
 });
