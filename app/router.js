de51df305dd0e582ad9eb201c94f6a16d5cd455d
Karen Freeman-Smith <karenfreemansmith>
2016-11-07 13:21:10 -0800

    setup main routes

---
+++ app/router.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+const Router = Ember.Router.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
+});
+
+Router.map(function() {
+  this.route('users');
+  this.route('meals');
+  this.route('foods');
+  this.route('goals');
+  this.route('activities');
+});
+
+export default Router;
---
+++ tests/unit/routes/meals-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:meals', 'Unit | Route | meals', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
