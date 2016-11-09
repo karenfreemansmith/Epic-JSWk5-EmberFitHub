e6d59d9679e4ec09be64c0c72d866aaa547ff1db
Dean Scelza and Garrett Price <garrett@>
2016-11-09 11:02:47 -0800

    add logo, seprate main image from forms and buttons below

--- app/models/meal.js
+++ app/models/meal.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   description: DS.attr(),
   mealtime: DS.attr(),
-  foods: DS.hasMany('food', {async: true}),
-  user: DS.belongsTo('user', { async: true })
+  foods: DS.attr()
+  // foods: DS.hasMany('food', {async: true}),
+  // user: DS.belongsTo('user', { async: true })
 });
