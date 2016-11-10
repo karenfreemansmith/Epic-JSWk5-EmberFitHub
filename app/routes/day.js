f6ebd4dc86b2fe3386a29134ed24fcd82f394f48
Dean Scelza and Garrett Price <dean+garrett@>
2016-11-09 17:08:48 -0800

    increased emoji size, delete meals route.

--- app/routes/day.js
+++ app/routes/day.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	actions: {
+		linktoActivities() {
+			this.transitionTo('activities');
+		},
+		linktoFood() {
+			this.transitionTo('foods');
+		}
+	}
 });
